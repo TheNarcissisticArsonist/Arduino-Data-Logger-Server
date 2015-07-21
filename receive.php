264eaee81949e020fb2a01231492bd46a913e867
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-20 21:16:59 -0400

    Added helpful comments.

--- receive.php
+++ receive.php
@@ -1,9 +1,11 @@
 <?php
 
+//Get information from the request
 $key  = $_REQUEST["key"];
 $id   = $_REQUEST["id"];
 $data = $_REQUEST["data"];
 
+//Various conditionals to make sure this is a valid request
 if($key != "superSecretPassword") {
   exit("Wrong password!");
 }
